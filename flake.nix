99f8a12ca0dbfcb78c840f88a0f4d13a2b60f706
Nick Dichev <github@dichev.email>
2023-05-13 23:07:08 -0700

    Bootstrap Janet development environment

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "Falling sand simulation written in Janet";
+
+  inputs = {
+    /* nixpkgs = { url = "github:NixOS/nixpkgs/nixos-22.11"; }; */
+    nixpkgs = { url = "github:nixos/nixpkgs/nixos-unstable"; };
+    flake-utils = { url = "github:numtide/flake-utils"; };
+  };
+
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    }:
+
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.default = pkgs.mkShell {
+        packages = (with pkgs; [ janet jpm ]) ++
+          pkgs.lib.optionals (pkgs.stdenv.isLinux) (with pkgs; []) ++ # Linux only
+          pkgs.lib.optionals (pkgs.stdenv.isDarwin) (with pkgs; [ darwin.libobjc ]) ++ # macOS only
+          (with pkgs.darwin.apple_sdk.frameworks; [
+            AudioToolbox
+            Carbon
+            Cocoa
+            CoreAudio
+            Foundation
+            IOKit
+            Kernel
+            OpenGL
+          ]);
+
+        shellHook = ''
+        '';
+      };
+    });
+}
